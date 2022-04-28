89dff03844e902ed50652d0345193b2ca3b1ac8b
S.Pons <snop.vlc@gmail.com>
2022-04-28 14:23:17 +0200

    Add Visual level editor 2D add game_data add level add main add setting add support add tile ignore graphics and levels

---
+++ 2-Visual level editor 2D python Tile/code/game_data.py
@@ -0,0 +1,12 @@
+# dictionary targets TILE files
+level_0 ={
+    'terrain': '../Levels/0/level_0_terrain.csv',
+    'coins':  '../Levels/0/level_0_coins.csv',
+    'fg palms': '../Levels/0/level_0_fg_palms.csv',
+    'bg palms': '../Levels/0/level_0_bg_palms.csv',
+    'crates': '../Levels/0/level_0_crates.csv',
+    'enemies': '../Levels/0/level_0_enemies.csv',
+    'cosntrainers': '../Levels/0/level_0_constrainers.csv',
+    'player': '../Levels/0/level_0_player.csv',
+    'grass': '../Levels/0/level_0_grass.csv'
+}
